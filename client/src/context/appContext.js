cc2f50659aa11b0eea88610234e69a0310d41345
juunweiii <122281113+juunweiii@users.noreply.github.com>
2024-07-06 16:28:31 +0800

    Revert "Express session"
    This reverts commit 3508d477206a47e1a5a8fb1f88cd02ceac6878f3.

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -101,16 +101,7 @@ const AppProvider = ({ children }) => {
       dispatch({ type: CLEAR_ALERT });
     }, 3000);
   };
-  // const addUserToLocalStorage = ({ user, session }) => {
-  //   localStorage.setItem('user', JSON.stringify(user));
-  //   localStorage.setItem('session', session);
-  // };
 
-  // const removeUserFromLocalStorage = () => {
-  //   localStorage.removeItem('user');
-  //   localStorage.removeItem('session');
-  // };
-  
   const setupUser = async ({ currentUser, endPoint, alertText }) => {
     dispatch({ type: SETUP_USER_BEGIN });
     try {
@@ -118,13 +109,12 @@ const AppProvider = ({ children }) => {
         `/api/v1/auth/${endPoint}`,
         currentUser
       );
-      //const { session } = data;
+
       const { user, location } = data;
       dispatch({
         type: SETUP_USER_SUCCESS,
         payload: { user, location, alertText },
       });
-      //addUserToLocalStorage({ session });
     } catch (error) {
       dispatch({
         type: SETUP_USER_ERROR,
@@ -140,7 +130,6 @@ const AppProvider = ({ children }) => {
   const logoutUser = async () => {
     await authFetch.get('/auth/logout');
     dispatch({ type: LOGOUT_USER });
-    //removeUserFromLocalStorage({ session });
   };
   const updateUser = async (currentUser) => {
     dispatch({ type: UPDATE_USER_BEGIN });
