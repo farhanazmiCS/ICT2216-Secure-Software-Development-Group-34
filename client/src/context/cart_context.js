903434b8807b6f527c37a1ca808470486f94aa89
DamianNJJ <115348708+DamianNJJ@users.noreply.github.com>
2024-07-08 13:01:37 +0800

    Updated for staff to process adoption requests

--- client/src/context/cart_context.js
+++ client/src/context/cart_context.js
@@ -5,6 +5,7 @@ import {
   REMOVE_CART_ITEM,
   CLEAR_CART,
   COUNT_CART_TOTALS,
+  ADD_ADOPTION_REQUEST,
 } from '../actions'
 
 const getLocalStorage = () => {
@@ -20,6 +21,7 @@ const initialState = {
   cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
+  adoption_requests: getLocalStorage('adoption_requests'),
 }
 
 const CartContext = React.createContext()
@@ -38,18 +40,27 @@ export const CartProvider = ({ children }) => {
   const clearCart = () => {
     dispatch({ type: CLEAR_CART })
   }
+
+  //Processing of adoption request by users
+  const addAdoptionRequest = (pet) => {
+    dispatch({ type: ADD_ADOPTION_REQUEST, payload: pet });
+  };
+
   useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(state.cart))
-    dispatch({ type: COUNT_CART_TOTALS })
-  }, [state.cart])
+    localStorage.setItem('cart', JSON.stringify(state.cart));
+    localStorage.setItem('adoption_requests', JSON.stringify(state.adoption_requests));
+    dispatch({ type: COUNT_CART_TOTALS });
+  }, [state.cart, state.adoption_requests]);
+
   return (
     <CartContext.Provider
-      value={{ ...state, addToCart, removeItem, clearCart }}
+      value={{ ...state, addToCart, removeItem, clearCart, addAdoptionRequest }}
     >
       {children}
     </CartContext.Provider>
-  )
+  );
 }
+
 // make sure use
 export const useCartContext = () => {
   return useContext(CartContext)
