578638b01c9c9f16b1ee1b70639f9aec02eade62
Waafi Adam <80448912+waafi-adam@users.noreply.github.com>
2024-07-08 10:04:00 +0800

    fix pet,product naming in app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const connectDB = require('./db/connect');
 // routers
 const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
-const productRouter = require('./routes/productRoutes');
+const petRouter = require('./routes/petRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const orderRouter = require('./routes/orderRoutes');
 
@@ -88,7 +88,7 @@ app.use(fileUpload());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
-app.use('/api/v1/products', productRouter);
+app.use('/api/v1/pets', petRouter);
 app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/orders', orderRouter);
 
