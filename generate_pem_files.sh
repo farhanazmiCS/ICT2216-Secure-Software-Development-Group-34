3f6fca636ef94f0d233b7edf13706a030851e351
Farhan Azmi <68798786+farhanazmiCS@users.noreply.github.com>
2024-07-09 00:41:27 +0800

    modified cert generator

--- generate_pem_files.sh
+++ generate_pem_files.sh
@@ -4,10 +4,10 @@
 mkdir -p certs
 
 # Generate key.pem
-openssl genrsa -out certs/key.pem 4096
+openssl genrsa -out key.pem 4096
 
 # Generate csr.pem (Certificate Signing Request)
-openssl req -new -key certs/key.pem -out certs/csr.pem -subj "/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=common.name"
+openssl req -new -key key.pem -out csr.pem -subj "/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=common.name"
 
 # Generate cert.pem (Self-signed Certificate)
-openssl x509 -req -days 365 -in certs/csr.pem -signkey certs/key.pem -out certs/cert.pem
+openssl x509 -req -days 365 -in csr.pem -signkey key.pem -out cert.pem
