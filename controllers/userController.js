daee406dfa73c8710ae09de24bab0c5a16f341bb
DamianNJJ <115348708+DamianNJJ@users.noreply.github.com>
2024-07-08 13:12:12 +0800

    Updated for admin to update roles

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,10 +7,20 @@ const {
   checkPermissions,
 } = require('../utils');
 
-const getAllUsers = async (req, res) => {
+/* const getAllUsers = async (req, res) => {
   console.log(req.user);
   const users = await User.find({ role: 'user' }).select('-password');
   res.status(StatusCodes.OK).json({ users });
+}; */
+
+//Getting all users' names and roles from the db
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({}, 'name role');
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ msg: 'Error fetching users' });
+  }
 };
 
 const getSingleUser = async (req, res) => {
@@ -59,12 +69,32 @@ const updateUserPassword = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'Success! Password Updated.' });
 };
 
+// Function to update roles
+const updateUserRole = async (req, res) => {
+  const { role } = req.body;
+  if (!role || !['admin', 'user', 'staff'].includes(role)) {
+    throw new CustomError.BadRequestError('Invalid role');
+  }
+
+  const userToUpdate = await User.findOne({ _id: req.params.id });
+
+  if (!userToUpdate) {
+    throw new CustomError.NotFoundError(`User not found with id ${req.params.id}`);
+  }
+
+  userToUpdate.role = role;
+  await userToUpdate.save();
+
+  res.status(StatusCodes.OK).json({ msg: `User role updated to ${role}` });
+};
+
 module.exports = {
   getAllUsers,
   getSingleUser,
   showCurrentUser,
   updateUser,
   updateUserPassword,
+  updateUserRole,
 };
 
 // update user with findOneAndUpdate
